f4eeac6de54a9f69131a75b5d8ba1b24741039f9
blacksun1 <simon@blacksun.cx>
2016-12-16 05:59:48 +0000

    Initial swearing

---
+++ README.md
@@ -0,0 +1,5 @@
+# blacksun1-quotes
+
+Stuff that blacksun1 says. For those who asked for them
+
+![Parental Advisory: Explicit Lyrics](pa.jpg)
---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports = module.exports = [
+    'What the fuck?',
+    'No, you\'re not listening to me',
+    'Are you fucking kidding me?',
+    'G\'day mate',
+    'Awesome',
+    'It\'s all good'
+];
