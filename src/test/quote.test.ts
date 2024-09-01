f5557674ee4ea1e60bf947805d8ec1f51da9ed6c
Simon Bruce <simon@blacksun.cx>
2024-09-01 21:55:27 +0930

    Adds github action workflow

--- src/test/quote.test.ts
+++ src/test/quote.test.ts
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import { expect } from 'chai';
-import Sut from '../index.js';
+import { quotes as Sut } from 'blacksun1-quotes';
 
 test('should return an array of strings', () => {
     expect(Sut).to.be.an('array').that.is.not.empty;
